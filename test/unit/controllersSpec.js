1acc7acc30b7dbe449db246804ccf2d7948729d9
Van Doan <vdoan09@gmail.com>
2014-08-26 16:34:56 -0400

    updated the index so that it's dynamic and accepts data from the controller, added testing to the mvc, had a serious bug with the syntax, missing quotes, and experimented with creating a table and inserting other data into the index. Made the test to fail to find 4 phones.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,3 +1,30 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+
+describe('controllers', function() {
+
+  it("should do something", function() {
+
+  });
+
+});
+
+
+describe('PhoneListCtrl', function(){
+
+	beforeEach(module('phonecatApp')); 
+
+	it('should create "phones" model with 3 phones', 
+inject(function($controller) {
+	var scope = {}, 
+		ctrl = $controller('PhoneListCtrl', 
+{$scope:scope});  
+
+	expect(scope.phones.length).toBe(4); 
+
+	expect(scope.name).toBe('World');
+	}));
+
+});
+
